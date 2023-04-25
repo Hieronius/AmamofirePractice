a3c7e0989c90a91d95b1ed7cf6e8bb964f50ad0a
Arsenty Halimovsky <ghost12@yandex.ru>
2023-04-25 20:25:30 +0300

    Create TableViewController

---
+++ AlamofirePractice/AlamofirePractice/MyTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MyTableViewController.swift
+//  AlamofirePractice
+//
+//  Created by Арсентий Халимовский on 25.04.2023.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // some code here
+    }
+}
