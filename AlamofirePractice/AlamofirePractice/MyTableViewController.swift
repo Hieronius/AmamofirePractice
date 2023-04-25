02edfbbe4d1d8cba8d3e008be471f69b04a52aeb
Arsenty Halimovsky <ghost12@yandex.ru>
2023-04-25 20:41:33 +0300

    Add datSource and Delegate methods

--- AlamofirePractice/AlamofirePractice/MyTableViewController.swift
+++ AlamofirePractice/AlamofirePractice/MyTableViewController.swift
@@ -8,9 +8,29 @@
 import UIKit
 
 class MyTableViewController: UITableViewController {
+    
+    var array = ["example"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // some code here
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        cell.textLabel?.text = array[indexPath.row]
+        
+        return cell
+    }
 }
+
